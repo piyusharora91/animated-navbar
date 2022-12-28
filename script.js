5c6d16aa51bb8251a6c36f085c70b83372e5e8ea
piyusharora91 <piyusharora596@gmail.com>
2022-12-28 15:57:46 +0530

    overflow disabled when menu is open and vice-versa along with menu close animation only when link text is clicked

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const body = document.getElementsByTagName('body');
+const body = document.getElementById('body');
 const menuBars = document.querySelector('#menu-bars');
 const overlay = document.querySelector('#overlay');
+const navLinksContainer = document.querySelectorAll('.nav-links-container');
 const navLinks = document.querySelectorAll('.nav-links');
 
 let openNav = false;
@@ -10,21 +11,25 @@ const toggleNav = () => {
     menuBars.classList.toggle('change');
     if (!openNav) {
         openNav = true;
+        body.style.overflow = 'hidden';
         overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
-        navLinks.forEach((link, index) =>
-            link.classList.replace(`slide-out-${index + 1}`, `slide-in-${index + 1}`)
+        navLinksContainer.forEach((linkContainer, index) =>
+            linkContainer.classList.replace(`slide-out-${index + 1}`, `slide-in-${index + 1}`)
         );
     }
     else {
         openNav = false;
+        body.style.overflow = 'initial';
         overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
-        navLinks.forEach((link, index) =>
-            link.classList.replace(`slide-in-${index + 1}`, `slide-out-${index + 1}`)
+        navLinksContainer.forEach((linkContainer, index) =>
+            linkContainer.classList.replace(`slide-in-${index + 1}`, `slide-out-${index + 1}`)
         );
     }
 }
 
-// Event Listeners
-menuBars.addEventListener('click', toggleNav);
-navLinks.forEach(link => link.addEventListener('click', toggleNav));
+window.addEventListener('DOMContentLoaded', () => {
+    // Event Listeners
+    menuBars.addEventListener('click', toggleNav);
+    navLinks.forEach(link => link.addEventListener('click', toggleNav));
+});
 
